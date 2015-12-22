ed4b9e76e68803f56754195d18490754483fa778
Devan Goodwin <dgoodwin@rm-rf.ca>
2015-12-22 19:29:53 -0400

    Scan directory recursively for images.

--- src/github.com/dgoodwin/gophoto/gophoto.go
+++ src/github.com/dgoodwin/gophoto/gophoto.go
@@ -2,9 +2,32 @@ package main
 
 import (
 	"fmt"
-	"github.com/dgoodwin/stringutil"
+	"os"
+	"path/filepath"
 )
 
+func visit(path string, f os.FileInfo, err error) error {
+	if !f.IsDir() && isImage(path) {
+		fmt.Printf("Visited: %s\n", path)
+		//fmt.Printf("  %d %s %s\n", f.Size(), f.Mode(), f.IsDir())
+	}
+	return nil
+}
+
+func isImage(path string) bool {
+	imageExtensions := map[string]bool{
+		".jpg": true,
+		".JPG": true,
+	}
+	if imageExtensions[filepath.Ext(path)] {
+		return true
+	}
+	return false
+}
+
 func main() {
-	fmt.Printf(stringutil.Reverse("Hello, world.\n"))
+	fmt.Printf("Hello, world.\n")
+	importDir := "/home/dev/Photos/2015/11"
+	err := filepath.Walk(importDir, visit)
+	fmt.Printf("Walk returned: %v\n", err)
 }
