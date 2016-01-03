a4ca8cde00a0b88d717bf281458c2476e04c49dc
Devan Goodwin <dgoodwin@rm-rf.ca>
2016-01-03 08:54:21 -0400

    Move file scan code to importer package.

--- gophoto.go
+++ gophoto.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 	"github.com/dgoodwin/gophoto/config"
 	"github.com/dgoodwin/gophoto/handlers"
+	"github.com/dgoodwin/gophoto/importer"
 	"github.com/gorilla/mux"
-	"image"
 	_ "image/jpeg"
 	_ "image/png"
 	"log"
@@ -13,41 +13,6 @@ import (
 	"os"
 	"path/filepath"
 )
-
-func checkFile(path string, f os.FileInfo, err error) error {
-	if !f.IsDir() && isImage(path) {
-		fmt.Printf("Visited: %s\n", path)
-		//fmt.Printf("  %d %s %s\n", f.Size(), f.Mode(), f.IsDir())
-		width, height := getImageDimension(path)
-		fmt.Println("  Width:", width, "  Height:", height)
-	}
-	return nil
-}
-
-func isImage(path string) bool {
-	imageExtensions := map[string]bool{
-		".jpg": true,
-		".JPG": true,
-	}
-	if imageExtensions[filepath.Ext(path)] {
-		return true
-	}
-	return false
-}
-
-func getImageDimension(imagePath string) (int, int) {
-	file, err := os.Open(imagePath)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%v\n", err)
-	}
-	defer file.Close()
-
-	image, _, err := image.DecodeConfig(file)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s: %v\n", imagePath, err)
-	}
-	return image.Width, image.Height
-}
 
 // Default location of this file when run in the Docker container:
 var DEFAULT_CONFIG string = "/go/src/app/devel-config.yml"
@@ -71,7 +36,7 @@ func main() {
 
 	fmt.Printf("Hello, world.\n")
 	importDir := "/import"
-	err := filepath.Walk(importDir, checkFile)
+	err := filepath.Walk(importDir, importer.CheckFile)
 	fmt.Printf("Walk returned: %v\n", err)
 
 	r := mux.NewRouter()
---
+++ importer/importer.go
@@ -0,0 +1,45 @@
+package importer
+
+import (
+	"fmt"
+	"image"
+	_ "image/jpeg"
+	_ "image/png"
+	"os"
+	"path/filepath"
+)
+
+func CheckFile(path string, f os.FileInfo, err error) error {
+	if !f.IsDir() && isImage(path) {
+		fmt.Printf("Visited: %s\n", path)
+		//fmt.Printf("  %d %s %s\n", f.Size(), f.Mode(), f.IsDir())
+		width, height := getImageDimension(path)
+		fmt.Println("  Width:", width, "  Height:", height)
+	}
+	return nil
+}
+
+func isImage(path string) bool {
+	imageExtensions := map[string]bool{
+		".jpg": true,
+		".JPG": true,
+	}
+	if imageExtensions[filepath.Ext(path)] {
+		return true
+	}
+	return false
+}
+
+func getImageDimension(imagePath string) (int, int) {
+	file, err := os.Open(imagePath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+	}
+	defer file.Close()
+
+	image, _, err := image.DecodeConfig(file)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s: %v\n", imagePath, err)
+	}
+	return image.Width, image.Height
+}
